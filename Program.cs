6ab1c8b7cb6ea1e8c8aa873aed3d81b161a3fbb5
Elina Goncharova <ElinaAl.28@yandex.ru>
2024-02-18 18:27:40 +1200

    Сделали код к 1 задаче (проверка квадрата)

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два целых числа и проверяет ,
+// является ли первое число квадратом второго
+
+int first = 25;
+int second = 5;
+
+if (second * second == first)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
